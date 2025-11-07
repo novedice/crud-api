45e5da5be6596bb550a29821db0850b8d4ef408f
novedice <natalia@Natalias-MacBook-Air.local>
2025-11-07 19:19:15 +0000

    feat: implement userDelete function

--- src/controllers/deleteUser.ts
+++ src/controllers/deleteUser.ts
@@ -1,6 +1,31 @@
 import { IncomingMessage, ServerResponse } from "node:http";
+import { users } from "../server";
+import { validate } from "uuid";
 
 export const deleteUser = async (
   request: IncomingMessage,
   response: ServerResponse,
-) => {};
+) => {
+  const { url } = request;
+  if (url) {
+    const urlParts = url.split("/").filter((part) => part !== "");
+    if (urlParts.length === 3) {
+      const userID = urlParts[2];
+      const thisIndex = users.findIndex((item) => item.id === userID);
+      const thisUser = users[thisIndex];
+      if (!validate(userID)) {
+        response.writeHead(400, { "Content-Type": "application/json" });
+        response.end(JSON.stringify("Invalid userID"));
+        return;
+      }
+      if (!thisUser) {
+        response.writeHead(404, { "Content-Type": "application/json" });
+        response.end(JSON.stringify(`User with id ${userID} not found`));
+        return;
+      }
+      users.splice(thisIndex, 1);
+      response.writeHead(200, { "Content-Type": "application/json" });
+      response.end(JSON.stringify(thisUser));
+    }
+  }
+};
